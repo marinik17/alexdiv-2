266b62cc89b24961e7757d15cd1b2ca89176de48
marinik17 <marinik08@gmail.com>
2018-03-31 23:23:42 +0200

    New files added

---
+++ js/animation.js
@@ -0,0 +1,82 @@
+// Changing the defaults
+window.sr = ScrollReveal({ reset: true });
+
+// Customizing a reveal set
+sr.reveal('.showcase-left', { 
+duration: 2000,
+viewFactor: 0.2,
+origin: 'left',
+});
+sr.reveal('.showcase-right', { 
+duration: 2000,
+viewFactor: 0.2,
+origin: 'right',
+});
+
+sr.reveal('.site1',{ 
+duration: 2000,
+viewFactor: 0.2,
+distance:'40px',
+origin: 'bottom',
+});
+
+sr.reveal('.site2',{ 
+duration: 2000,
+delay:100,
+viewFactor: 0.2,
+distance:'40px',
+origin: 'bottom',
+});
+
+sr.reveal('.site3',{ 
+duration: 2000,
+delay:200,
+viewFactor: 0.2,
+distance:'40px',
+origin: 'bottom',
+});
+
+sr.reveal('.site4', { 
+duration: 2000,
+delay:300,
+viewFactor: 0.2,
+distance:'40px',
+origin: 'bottom',
+});
+
+
+sr.reveal('.site5', { 
+duration: 2000,
+delay:400,
+viewFactor: 0.2,
+distance:'40px',
+origin: 'bottom',
+});
+
+
+sr.reveal('.site6', { 
+duration: 2000,
+delay:500,
+viewFactor: 0.2,
+distance:'40px',
+origin: 'bottom',
+});
+sr.reveal('.text', { 
+duration: 2000,
+delay:600,
+viewFactor: 0.2,
+distance:'40px',
+origin: 'bottom',
+});
+sr.reveal('#my_form', { 
+duration: 2000,
+viewFactor: 0.2,
+distance:'20px',
+origin: 'bottom',
+});
+sr.reveal('.info', { 
+duration: 2000,
+viewFactor: 0.2,
+distance:'20px',
+origin: 'bottom',
+});
